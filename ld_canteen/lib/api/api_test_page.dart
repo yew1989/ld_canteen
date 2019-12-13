932e0adb7d9eb7e2af421e72405e26a7854d818e
leo <>
2019-12-13 14:39:42 +0800

    接口测试程序

--- ld_canteen/lib/api/api_test_page.dart
+++ ld_canteen/lib/api/api_test_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ld_canteen/api/api.dart';
+import 'package:ld_canteen/model/dish.dart';
 
 class ApiTestPage extends StatefulWidget {
   @override
@@ -7,9 +9,11 @@ class ApiTestPage extends StatefulWidget {
 
 class _ApiTestPageState extends State<ApiTestPage> {
 
+  // UI 初始化
   List<String> itemTitles = ['菜品列表','新增菜品','修改菜品','删除菜品'];
   List<Color> itemColors = [Colors.blue,Colors.green,Colors.orange,Colors.red];
 
+  // 单元列表项
   Widget listTitle(int index) {
     var itemTitle = itemTitles[index]; 
     var itemColor = itemColors[index];
@@ -17,9 +21,7 @@ class _ApiTestPageState extends State<ApiTestPage> {
       title: FlatButton(
         color: itemColor,
         child: Text('$itemTitle',style: TextStyle(color:Colors.white,fontSize: 15)),
-        onPressed: (){
-
-        },
+        onPressed: ()=>handleTestAPI(index),
       ),
     );
   }
@@ -36,4 +38,95 @@ class _ApiTestPageState extends State<ApiTestPage> {
       ),
     );
   }
+
+  // 请求测试
+  void handleTestAPI(int index) {
+    switch (index) {
+
+      // 获取菜品列表
+      case 0:
+        {
+          
+          API.getDishList((List<Dish> dishes,String msg){
+            
+            debugPrint(msg);
+            debugPrint(dishes.map((f)=>f.toJson()).toList().toString());
+
+          }, (String msg){
+
+            debugPrint(msg);
+
+          },order:'sort',limit: 20,skip: 0);
+        }
+        break;
+
+        // 新增菜品
+        case 1:
+        {
+            // 菜品对象
+            var dish = Dish();
+            dish.name = '青椒肉丝';
+            dish.price = '8.5';
+            dish.isShow = true;
+            dish.sort = 8;
+
+            API.addDish(dish,(String objectId,String msg){
+            
+            debugPrint(msg);
+            debugPrint(objectId);
+
+          }, (String msg){
+
+            debugPrint(msg);
+
+          });
+        }
+        break;
+
+        // 修改菜品
+        case 2:
+        {
+
+          final objectId = '5df3304543c2570080cdb950';
+          // 菜品对象
+          var dish = Dish();
+          dish.name = '肉末茄子';
+          dish.price = '3.5';
+          dish.isShow = true;
+          dish.sort = 4;
+
+          API.updateDish(objectId,dish,(String objectId,String msg){
+            
+            debugPrint(msg);
+            debugPrint(objectId);
+
+          }, (String msg){
+
+            debugPrint(msg);
+
+          });
+        }
+        break;
+
+        // 删除菜品
+        case 3:
+        {
+          final objectId = '5df330430a8a84007ffafa2e';
+
+          API.deleteDish(objectId,(String msg){
+            
+            debugPrint(msg);
+
+          }, (String msg){
+
+            debugPrint(msg);
+
+          });
+        }
+        break;
+
+      default:
+    }
+  }
+
 }
